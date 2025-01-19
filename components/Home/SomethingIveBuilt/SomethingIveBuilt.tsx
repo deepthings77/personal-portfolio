965dcb643bf6704543c1fb2c7558fa2d43f838f5
Deepak Kumawat <117026690+deepthings77@users.noreply.github.com>
2025-01-19 11:26:08 +0530

    Update SomethingIveBuilt.tsx
    added more project

--- components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
+++ components/Home/SomethingIveBuilt/SomethingIveBuilt.tsx
@@ -29,6 +29,134 @@ export default function SomethingIveBuilt() {
       </div>
 
       <div className="flex flex-col   xl:space-y-36 space-y-8 md:space-y-28">
+          {/* // ?  Project  3*/}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              <Link href={"https://github.com/deepthings77/boxhole"}>
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAsecondary 
+           transition-opacity opacity-10 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </Link>
+
+              <Img src={"/img/boxhole.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/img/boxhole.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">Boxhole</span>
+                <Link href={"https://github.com/deepthings77/boxhole"}>
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Boxhole-The Ultimate blackhole of storage
+                  </span>
+                </Link>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+                <span className="text-AAsecondary">Boxhole</span> lets you use YouTube as a <span className="text-AAsecondary">Infinite Cloud Storage</span>. It embeds any files like - Images, Videos, Documents, etc into a video. The video can be uploaded to YouTube and the file can be retrieved from the video. A expansion & compression technique is used to defeat YouTube&apos;s compression algorithm. The file can be retrieved from the video without any loss of data.
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Python</span>
+                <span className="pr-4 z-10">C++</span>
+                <span className="pr-4 z-10">Docker</span>
+                {/* <span className="pr-4 z-10">TypeScript</span> */}
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <GithubIcon link="https://github.com/deepthings77/boxhole" />
+                {/* <ExternalLink url={"typing"} router={router} /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+          {/* // ?  Project  3*/}
+        <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
+          {/* Left image */}
+          <div
+            className="hidden bg-AAprimary z-10  py-4 
+          absolute md:grid grid-cols-12 w-full h-full  content-center"
+          >
+            <div className="relative rounded w-full h-full col-span-7 ">
+              <Link href={"https://github.com/deepthings77/boxhole"}>
+                <div
+                  // onClick={}
+                  className="absolute w-full h-full rounded bg-AAsecondary 
+           transition-opacity opacity-10 hover:opacity-0 hover:cursor-pointer duration-300"
+                ></div>
+              </Link>
+
+              <Img src={"/img/boxhole.png"} alt={"Project Screen shot"} className={`w-full rounded h-full `} />
+            </div>
+          </div>
+
+          {/* right Content */}
+          <div className=" md:absolute py-4  md:grid md:grid-cols-12 w-full h-full  content-center ">
+            {/* background for text in mobile responsive */}
+            <div className="absolute w-full h-full bg-opacity-70 z-0">
+              <div className="relative w-full h-full">
+                <div className="absolute w-full h-full bg-AAsecondary opacity-10 z-10"></div>
+                <div className="absolute w-full h-full bg-AAprimary opacity-80 z-10"></div>
+                <Img src={"/img/boxhole.png"} alt={"Project Screen shot"} className={`w-full h-full `} />
+              </div>
+            </div>
+
+            <div
+              className="px-8 pt-8 sm:pt-12 md:py-0 xl:col-span-6 xl:col-start-7 col-start-5 
+            col-span-8 flex flex-col items-start md:items-end space-y-3"
+            >
+              <div className="flex flex-col space-y-1 md:items-end z-10">
+                <span className="text-AAsecondary text-base">Boxhole</span>
+                <Link href={"https://github.com/deepthings77/boxhole"}>
+                  <span className=" md:text-gray-200 text-AAsecondary font-bold text-xl hover:cursor-pointer">
+                    Boxhole-The Ultimate blackhole of storage
+                  </span>
+                </Link>
+              </div>
+              <div className="w-full md:bg-AAtertiary rounded-md py-6 md:p-6  z-10">
+                <p className="text-gray-300 md:text-gray-400 text-left md:text-right">
+                <span className="text-AAsecondary">Boxhole</span> lets you use YouTube as a <span className="text-AAsecondary">Infinite Cloud Storage</span>. It embeds any files like - Images, Videos, Documents, etc into a video. The video can be uploaded to YouTube and the file can be retrieved from the video. A expansion & compression technique is used to defeat YouTube&apos;s compression algorithm. The file can be retrieved from the video without any loss of data.
+                </p>
+              </div>
+              <ul
+                className="flex flex-wrap w-full text-gray-300 md:text-gray-400
+               text-sm font-Text2 md:justify-end"
+              >
+                <span className="pr-4 z-10">Python</span>
+                <span className="pr-4 z-10">C++</span>
+                <span className="pr-4 z-10">Docker</span>
+                {/* <span className="pr-4 z-10">TypeScript</span> */}
+              </ul>
+              <div className="z-10 flex fle-row space-x-5 ">
+                <GithubIcon link="https://github.com/deepthings77/boxhole" />
+                {/* <ExternalLink url={"typing"} router={router} /> */}
+              </div>
+            </div>
+          </div>
+        </div>
         {/* // ?  Project  1*/}
         <div data-aos="fade-up" className="relative md:grid md:grid-cols-12 w-full md:h-96 ">
           {/* Left image */}
